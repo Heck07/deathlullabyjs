8ee68a43bc03896bf4f21eb32fe984726e05ca92
Heck07 <roms904@yahoo.fr>
2024-10-29 12:06:44 +0100

    admin

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
