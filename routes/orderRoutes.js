35abdebb2dc95f7c7d53e17f03522f43590ad8b9
Heck07 <roms904@yahoo.fr>
2024-11-02 19:49:35 +0100

    strip

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,16 +3,7 @@ const router = express.Router();
 const orderController = require('../controllers/orderController');
 const roleMiddleware = require('../middlewares/roleMiddleware');
 
-// Récupérer toutes les commandes
-router.get('/', roleMiddleware('admin'), orderController.getAllOrders);
-
 // Créer une nouvelle commande
 router.post('/create', orderController.createOrder);
-
-// Mettre à jour une commande
-router.put('/:id', roleMiddleware('admin'), orderController.updateOrder);
-
-// Supprimer une commande
-router.delete('/:id', roleMiddleware('admin'), orderController.deleteOrder);
 
 module.exports = router;
