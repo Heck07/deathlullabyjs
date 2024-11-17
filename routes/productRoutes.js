f05ab034bd09519a0f67e1af718113d32d231294
Heck07 <roms904@yahoo.fr>
2024-11-17 18:18:25 +0100

    Product details

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,12 +8,12 @@ const authenticateToken = require('../middlewares/authMiddleware');
 const roleMiddleware = require('../middlewares/roleMiddleware');
 const upload = require('../config/uploadConfig'); // Importer le middleware Multer configuré
 
-router.get('/:id', productController.getProductById);
-
-// Récupérer tous les produits
-router.get('/', productController.getAllProducts);
+// Récupérer les produits
 router.get('/details', productController.getAllProductsDetails);
+router.get('/', productController.getAllProducts);
+
 router.get('/:id/images', productController.getProductImages);
+router.get('/:id', productController.getProductById);
 
 
 // Ajouter un produit (accès réservé aux administrateurs)
@@ -33,7 +33,6 @@ router.put(
   productController.updateProduct
 );
 
-
 // Supprimer un produit (accès réservé aux administrateurs)
 router.delete('/:id', authenticateToken, roleMiddleware('admin'), productController.deleteProduct);
 
@@ -48,6 +47,5 @@ router.post('/:id/sizes', authenticateToken, roleMiddleware('admin'), sizeContro
 router.get('/:id/sizes', sizeController.getSizesByProductId);
 router.put('/sizes/:sizeId', authenticateToken, roleMiddleware('admin'), sizeController.updateSize);
 router.delete('/sizes/:sizeId', authenticateToken, roleMiddleware('admin'), sizeController.deleteSize);
-
 
 module.exports = router;
